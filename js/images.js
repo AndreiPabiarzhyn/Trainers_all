f61ad6dde6ab57542d54c5818ad2d7972d556f4f
admin <a.pobyarzhin@gmail.com>
2025-12-03 00:26:25 +0100

    M8L1 AI Trainer initial upload

---
+++ js/images.js
@@ -0,0 +1,10 @@
+export const IMAGES = {
+  red:      "assets/img/backpack_red.png",
+  blue:     "assets/img/backpack_blue.png",
+  toy:      "assets/img/backpack_toy.png",
+  black:    "assets/img/backpack_black.png",
+  sport:    "assets/img/backpack_sport.png",
+  kid:      "assets/img/backpack_kid.png",
+  bag:      "assets/img/bag.png",
+  suitcase: "assets/img/suitcasesuitcase.png"
+};
